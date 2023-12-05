b780265a947a2f5c366d10150067f93856577dda
Sebastian <seb02tian20@gmail.com>
2023-12-05 16:54:15 -0700

    basic template

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    context = {
+        'name': 'Kappa'.upper(),
+    }
+    return render(request, 'index.html', context)
