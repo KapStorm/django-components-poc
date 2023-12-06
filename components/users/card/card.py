d7b74e71c9cb9ac90d76c595adcd21299d6a97b2
Sebastian <seb02tian20@gmail.com>
2023-12-05 17:19:42 -0700

    basic card component

---
+++ components/users/card/card.py
@@ -0,0 +1,14 @@
+from django_components import component
+
+
+@component.register('card')
+class CardComponent(component.Component):
+    template_name = 'users/card/card.html'
+
+    def get_context_data(self, user):
+        return {
+            'user': user
+        }
+
+    class Media:
+        css = 'users/card/style.css'
--- users/views.py
+++ users/views.py
@@ -3,7 +3,26 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    mocked_users = [
+        {
+            'name': 'Kappa',
+            'position': 'Developer',
+        },
+        {
+            'name': 'John Doe',
+            'position': 'Designer',
+        },
+        {
+            'name': 'Foo',
+            'position': 'Manager',
+        },
+        {
+            'name': 'Bar',
+            'position': 'Tester',
+        },
+    ]
+
     context = {
-        'name': 'Kappa'.upper(),
+        'users': mocked_users,
     }
     return render(request, 'index.html', context)
